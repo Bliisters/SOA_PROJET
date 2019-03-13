016af3fdd591d7417d0e6f27abb095dfe5c58ea4
Bliisters <quentin.flegeau@orange.fr>
2019-03-13 14:27:49 +0100

    Initial Commit for Frontend (base)

---
+++ Frontend/search.js
@@ -0,0 +1,7 @@
+/*function rechercherDecision() {
+    
+    var id = document.getElementById('id');
+    window.location.href=("http://ensibs2019-decisions-backoffice.93a47f09d45d4633a572.westeurope.aksapp.io/api/decisions/"+id);
+    
+  }
+  */
