037f32cbc1bf80067949b4f38b11d4414139d3e5
unknown <thibbbaut2975097@gmail.com>
2019-03-13 17:40:36 +0100

    client pour copie/colle

---
+++ JWT_TokenDealer/client.py
@@ -0,0 +1,12 @@
+import zmq
+import sys
+
+port = "5556"
+context = zmq.Context()
+print("Connecting to server...")
+socket = context.socket(zmq.REQ)
+socket.connect ("tcp://localhost:%s" % port)
+socket.send_string("Hello")
+#  Get the reply.
+message = socket.recv()
+print("Received reply [", message, "]")
